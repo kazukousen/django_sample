ae7b71f342627d8ecf017fa3bcff1772b05c7202
kazukousen <i101330@gm.ishikawa-nct.ac.jp>
2017-04-12 17:56:33 +0900

    Customize Admin

--- bbs/admin.py
+++ bbs/admin.py
@@ -1,5 +1,15 @@
 from django.contrib import admin
 from .models import Post, Comment
 
-admin.site.register(Post)
-admin.site.register(Comment)
+class CommentInline(admin.TabularInline):
+    model = Comment
+    extra = 1
+
+class PostAdmin(admin.ModelAdmin):
+    inlines = [CommentInline]
+
+class CommentAdmin(admin.ModelAdmin):
+    inlines = [CommentInline]
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(Comment, CommentAdmin)
