b9234177291b609c80e4589ff2a4fc5a384de8a2
kazukousen <i101330@gm.ishikawa-nct.ac.jp>
2017-04-19 10:49:27 +0900

    applied django-mptt

--- bbs/models.py
+++ bbs/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.urls import reverse
+from mptt.models import MPTTModel, TreeForeignKey
 
 class Post(models.Model):
     title = models.CharField(max_length=255)
@@ -17,26 +18,21 @@ class Post(models.Model):
         return reverse('bbs:post_detail', args=[self.id])
 
     def get_comments(self):
-        comments = self.comment_set.order_by('path')
+        comments = self.comment_set.all()
         return comments
 
-class Comment(models.Model):
+class Comment(MPTTModel):
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
-    path = models.TextField(null=False, default='')
     body = models.TextField()
     body_html = models.TextField(editable=False)
     pub_date = models.DateTimeField(auto_now_add=True)
     updated_date = models.DateTimeField(auto_now=True)
     bookmark_count = models.IntegerField(default=0)
     rating_score = models.IntegerField(default=0)
-    path_depth = models.PositiveIntegerField(editable=False, default=1)
+    parent = TreeForeignKey('self', null=True, blank=True, related_name='children', db_index=True)
 
     def __str__(self):
         return self.body[:20]
 
-    def save(self, *args, **kwargs):
-        self.path_depth = len(self.path) - len(self.path.replace('/', '')) - 1
-        return super(Comment, self).save(*args, **kwargs)
-
-    def path_depth_range(self):
-        return range(1, self.path_depth)
+    class MPTTMeta:
+        order_insertion_by = ['-rating_score']
