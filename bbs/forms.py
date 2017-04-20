9871f5672f7ae75af37be1fc22209f9b61c34cb1
kazukousen <i101330@gm.ishikawa-nct.ac.jp>
2017-04-20 17:39:07 +0900

    [WIP] vue.js導入

--- bbs/forms.py
+++ bbs/forms.py
@@ -1,9 +1,15 @@
 from django import forms
 from .models import Post, Comment
 
+class EditorTextarea(forms.Textarea):
+    template_name = 'bbs/textarea.html'
+
+    def __init__(self, attrs=None):
+        super(EditorTextarea, self).__init__(attrs)
+
 class PostForm(forms.ModelForm):
     title = forms.CharField()
-    description = forms.CharField(widget=forms.Textarea)
+    description = forms.CharField(widget=EditorTextarea)
 
     class Meta:
         model = Post
---
+++ bbs/static/bbs/markdown.js
@@ -0,0 +1,16 @@
+var editor = new Vue({
+  el: '#editor',
+  data: {
+    input: ''
+  },
+  computed: {
+    compiledMarkdown: function () {
+      return marked(this.input, { sanitize: true })
+    }
+  },
+  methods: {
+    update: _.debounce(function (e) {
+      this.input = e.target.value
+    }, 300)
+  }
+})
